32ae27b7e4a89f60ea6cf20ed63c773697f17a35
AskerovAdil <Askerov121099@mail.ru>
2023-06-21 00:15:02 +0700

    refactor: add automapper dto

---
+++ API/DirectoryAPI/Models/CatalogItemDto.cs
@@ -0,0 +1,11 @@
+﻿namespace DirectoryAPI.Models
+{
+    public class CatalogItemDto
+    {
+        public CatalogItemDto(string name) { 
+            this.Name = name;
+        }
+        public string Name { get; set; }
+        public int? ParentId { get; set; }
+    }
+}
---
+++ API/DirectoryAPI/Profiles/AutoMapperProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using DataAccess.Entities;
+using DirectoryAPI.Models;
+
+namespace DirectoryAPI.Profiles
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<CatalogItemDto, CatalogItem>();
+        }
+    }
+}

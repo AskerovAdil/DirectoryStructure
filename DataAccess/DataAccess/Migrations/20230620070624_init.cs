c3bfc7f3702062349a8404a23a6f50ca125206b1
AskerovAdil <Askerov121099@mail.ru>
2023-06-21 12:59:41 +0700

    init: add migrations db

---
+++ DataAccess/DataAccess/Migrations/20230620070624_init.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CatalogItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CatalogItemId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CatalogItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CatalogItems_CatalogItems_CatalogItemId",
+                        column: x => x.CatalogItemId,
+                        principalTable: "CatalogItems",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CatalogItems_CatalogItemId",
+                table: "CatalogItems",
+                column: "CatalogItemId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CatalogItems");
+        }
+    }
+}
---
+++ DataAccess/DataAccess/Migrations/20230620115108_changeCatalogItem.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    public partial class changeCatalogItem : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CatalogItems_CatalogItems_CatalogItemId",
+                table: "CatalogItems");
+
+            migrationBuilder.RenameColumn(
+                name: "CatalogItemId",
+                table: "CatalogItems",
+                newName: "ParentId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_CatalogItems_CatalogItemId",
+                table: "CatalogItems",
+                newName: "IX_CatalogItems_ParentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CatalogItems_CatalogItems_ParentId",
+                table: "CatalogItems",
+                column: "ParentId",
+                principalTable: "CatalogItems",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CatalogItems_CatalogItems_ParentId",
+                table: "CatalogItems");
+
+            migrationBuilder.RenameColumn(
+                name: "ParentId",
+                table: "CatalogItems",
+                newName: "CatalogItemId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_CatalogItems_ParentId",
+                table: "CatalogItems",
+                newName: "IX_CatalogItems_CatalogItemId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CatalogItems_CatalogItems_CatalogItemId",
+                table: "CatalogItems",
+                column: "CatalogItemId",
+                principalTable: "CatalogItems",
+                principalColumn: "Id");
+        }
+    }
+}
